1a39af6e4cdfdc7ffeeb7f70619a468db788fe54
Cyril Mazouyer <cyril.mazouyer@gmail.com>
2018-01-04 22:30:11 +0100

    test : ajouter un champ prénom dans le formulaire

--- lib/php/admin/adminfunction.php
+++ lib/php/admin/adminfunction.php
@@ -2207,9 +2207,9 @@ En cas de question, vous pouvez trouver des informations sur https://github.com/
 				$prop_poi = mysql_real_escape_string($_POST['prop_poi']);
 				$adherent_poi = mysql_real_escape_string($_POST['adherent_poi']);
 				$adherentfirstname_poi = mysql_real_escape_string($_POST['adherentfirstname_poi']);
-				$latitude_poi = mysql_real_escape_string($_POST['latitude_poi']);
-				$longitude_poi = mysql_real_escape_string($_POST['longitude_poi']);
-				$subcategory_id_subcategory = mysql_real_escape_string($_POST['subcategory_id_subcategory']);
+				$latitude_poi = $_POST['latitude_poi'];
+				$longitude_poi = $_POST['longitude_poi'];
+				$subcategory_id_subcategory = $_POST['subcategory_id_subcategory'];
 				
 				$sql = "SELECT lib_subcategory FROM subcategory WHERE id_subcategory = ".$subcategory_id_subcategory;
 				$result = mysql_query($sql);
@@ -2332,15 +2332,7 @@ En cas de question, vous pouvez trouver des informations sur https://github.com/
 				if (DEBUG){
 					error_log(date("Y-m-d H:i:s") . " " .__FUNCTION__ . " - place locations - ".$commune_id_commune.", " .$lib_commune.", " .$pole_id_pole.", " .$lib_pole."\n", 3, LOG_FILE);
 				}
-
-				// Construction de la référence du POI
-				$sql = "SELECT IFNULL(max(seq), 0) as last_seq FROM poireferences WHERE year = " . date("y") . " AND month = " . date("m");
-				$result = mysql_query($sql);
-				$row = mysql_fetch_assoc( $result );
-				$ref_sequence = $row['last_seq']+1;
-				$ref_poi = "F" . date('ym') . str_pad($ref_sequence, 2, '0', STR_PAD_LEFT);
-
-				$sql = "INSERT INTO poi (adherent_poi, adherentfirstname_poi, ref_poi, priorite_id_priorite, quartier_id_quartier, pole_id_pole, lib_poi, mail_poi, tel_poi, num_poi, rue_poi, communename_poi, commune_id_commune, desc_poi, prop_poi, subcategory_id_subcategory, display_poi, fix_poi, datecreation_poi, geolocatemode_poi, moderation_poi, geom_poi, status_id_status, photo_poi) VALUES ('$adherent_poi', '$adherentfirstname_poi', '$ref_poi', $priorityId, $quartier_id_quartier, $pole_id_pole, '$lib_subcategory', '$mail_poi', '$tel_poi', '$num_poi', '$rue_poi', '$communename_poi', $commune_id_commune, '$desc_poi', '$prop_poi', $subcategory_id_subcategory , TRUE, FALSE, '$datecreation_poi', 1, $moderationFlag, GeomFromText('POINT(".$longitude_poi." ".$latitude_poi.")'), 5, '$url_photo')";
+				$sql = "INSERT INTO poi (adherent_poi, adherentfirstname_poi, priorite_id_priorite, quartier_id_quartier, pole_id_pole, lib_poi, mail_poi, tel_poi, num_poi, rue_poi, commune_id_commune, desc_poi, prop_poi, subcategory_id_subcategory, display_poi, fix_poi, datecreation_poi, geolocatemode_poi, moderation_poi, geom_poi, status_id_status, photo_poi) VALUES ('$adherent_poi', '$adherentfirstname_poi', $priorityId, $quartier_id_quartier, $pole_id_pole, '$lib_subcategory', '$mail_poi', '$tel_poi', '$num_poi', '$rue_poi', $commune_id_commune, '$desc_poi', '$prop_poi', $subcategory_id_subcategory , TRUE, FALSE, '$datecreation_poi', 1, $moderationFlag, GeomFromText('POINT(".$longitude_poi." ".$latitude_poi.")'), 5, '$url_photo')";
 				
 				if (DEBUG){
 					error_log(date("Y-m-d H:i:s") . " " .__FUNCTION__ . " - createPublicPoi - Requete d'insertion sql = ".$sql."\n", 3, LOG_FILE);
@@ -2408,7 +2400,7 @@ Cordialement, l'Association ".VELOBS_ASSOCIATION." :)";
 				}
 				}else{
 					
-					$infoPOI = "Repere : $num_poi\nMail : $mail_poi\nTel : $tel_poi\nRue : $rue_poi\nCommune : $communename_poi\nDescription : $desc_poi\nProposition : $prop_poi\nNom : $adherent_poi\nPrenom : $adherentfirstname_poi\nLatitude : $latitude_poi\nLongitude : $longitude_poi\n Categorie : $subcategory_id_subcategory";
+					$infoPOI = "Repere : $num_poi\nMail : $mail_poi\nTel : $tel_poi\nRue : $rue_poi\nDescription : $desc_poi\nProposition : $prop_poi\nNom : $adherent_poi\nPrenom : $adherentfirstname_poi\nLatitude : $latitude_poi\nLongitude : $longitude_poi\n Categorie : $subcategory_id_subcategory";
 					sendMail(MAIL_FROM,"Erreur méthode createPublicPoi", "Erreur = " .  $return['pb'] . "\n" . $infoPOI);
 					$return['success'] = false;
 					$return['pb'] = "Erreur lors de l'ajout de l'observation : " . $return['pb'];
